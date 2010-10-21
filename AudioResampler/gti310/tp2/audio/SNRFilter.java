a873ffddb46ce04995ccdeb284231c9759048f4c
chebli.r@gmail.com <chebli.r@gmail.com@11dbf93d-3370-3516-7c29-4c7ee8b01536>
2010-10-21 15:11:52 +0000

    Modifications à la boucle logarithmique

--- AudioResampler/gti310/tp2/audio/SNRFilter.java
+++ AudioResampler/gti310/tp2/audio/SNRFilter.java
@@ -46,25 +46,37 @@ public class SNRFilter implements AudioFilter {
 				int nbEch=HandlerCompares[i].getDataChunkSize();
 
 				System.out.println("Les deux fichiers sont mono, 8 bits et de même taille  "+(nbEch+44)+"Bytes");				
-
+				
+				//valeur originale
+				if(val1 == 0){
+					for (int j=0; j<nbEch;j++){
+						double EchOriginal =(double)HandlerComparant.getData();
+						//val1 = val1+Math.pow(EchOriginal, 2);
+						val1 = val1 + EchOriginal;
+					}
+				}
+				
+				//valeur comparée
 				for(int j=0; j<nbEch;j++){
-                    
-					double EchOriginal =(double)HandlerComparant.getData();					
-					double EchCompare = (double)HandlerCompares[i].getData();
-					
-					val1 = val1+Math.pow(EchOriginal, 2);
-					val2 = val2+Math.pow((EchOriginal-EchCompare), 2);
-					//System.out.println("Val1 "+val1+" Val2 "+val2);
-					
+                    double EchCompare = (double)HandlerCompares[i].getData();
+					//System.out.println("EchOriginal "+EchOriginal+" EchCompare "+EchCompare+" j  "+j);
+					val2 = val2 + EchCompare;
+					//val2 = val2+Math.pow((EchCompare), 2);
+					//System.out.println("Val1 "+val1+" Val2 "+val2);					
 					//System.out.println("Process@SNRFilter : i : "+i+" j : "+j+" SNR : "+SNRinter);
 
 				}
+				//formule a lintérieur du LOG
+				double top = Math.pow(val1, 2);
+				double bottom = Math.pow(val1-val2, 2);
+				System.out.println("Val1 "+top+" Val2 "+bottom);
 				
-				System.out.println("Val1 "+val1+" Val2 "+val2);
-				SNRinter= 10 * Math.log10(val1/val2);
+				SNRinter= 10 * Math.log10(top/bottom);
+				val2=0;
 				System.out.println("SNRinter.."+SNRinter);
 				System.out.println("I.."+i);
 				tableauSNR[i] = SNRinter;
+				
 
 			}
 
