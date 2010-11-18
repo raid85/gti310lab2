8c3b50942ec8077dcd718ded585d712613afe529
chebli.r@gmail.com <chebli.r@gmail.com@11dbf93d-3370-3516-7c29-4c7ee8b01536>
2010-11-18 20:12:57 +0000

    Enelever les <static> partout dans la classe RGB2YUV Continuation de la classe de test

--- SqueezeLightMediaCodec/gti310/tp4/ConvertRGB2YUV.java
+++ SqueezeLightMediaCodec/gti310/tp4/ConvertRGB2YUV.java
@@ -2,9 +2,9 @@ package gti310.tp4;
 
 public class ConvertRGB2YUV {
 
-	public static int[][][] matriceYUV = null;
+	private int[][][] matriceYUV = null;
 
-	public static int[][][] convert_RGB_to_YUV(int[][][] matriceRGB){
+	public int[][][] convert_RGB_to_YUV(int[][][] matriceRGB){
 
 		//y = 0.299R + 0.587G + 0.114B
 		//u = 0.492(B-Y)
@@ -18,12 +18,15 @@ public class ConvertRGB2YUV {
 			for (j=0; j<n; j++){
 				for (k=0; k<n; k++){
 					if ( i==0){
+						//on rempli la matrice des Y
 						matriceYUV[i][j][k] = get_Y(matriceRGB[0][j][k], matriceRGB[1][j][k], matriceRGB[2][j][k]);
 					}
 					if ( i==1){
+						//on rempli la matrice des U
 						matriceYUV[i][j][k] = get_U(matriceRGB[2][j][k], matriceYUV[0][j][k] );
 					}
 					if ( i==2){
+						//on rempli la matrice des V
 						matriceYUV[i][j][k] = get_V(matriceRGB[0][j][k], matriceYUV[0][j][k] );
 					}
 				}
@@ -34,16 +37,20 @@ public class ConvertRGB2YUV {
 
 	}
 
-	public static int get_Y(double R, double G, double B){
+	public int get_Y(double R, double G, double B){
 		int Y = (int) Math.round(0.299*R + 0.587*G + 0.114*B);
 		return Y;
 	}
-	public static int get_U(double B, double Y){
+	public int get_U(double B, double Y){
 		int U = (int) Math.round(0.492*(B-Y));
 		return U;
 	}
-	public static int get_V(double R, double Y){
+	public int get_V(double R, double Y){
 		int V = (int) Math.round(0.877*(R-Y));
 		return V;
+	}
+
+	public int[][][] getMatriceYUV() {
+		return matriceYUV;
 	}
 }
