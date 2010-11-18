79ca2b0c3ce00ad7fe8226b3535db7acf13b5826
chebli.r@gmail.com <chebli.r@gmail.com@11dbf93d-3370-3516-7c29-4c7ee8b01536>
2010-11-18 19:02:37 +0000

    debut de la calsse de test de RGB2YUV

---
+++ trunk/SqueezeLightMediaCodec/gti310/tp4/TestConvertRGB2YUV.java
@@ -0,0 +1,26 @@
+package gti310.tp4;
+
+import junit.framework.TestCase;
+
+public class TestConvertRGB2YUV extends TestCase {
+	
+	public void testConvertRGB2YUV(){
+		
+		ConvertRGB2YUV testeur=new ConvertRGB2YUV();
+		
+		
+	}
+	public void testconvert_RGB_to_YUV(){
+		
+	}
+	public void testget_Y() {
+
+	}
+	public void testget_U() {
+
+	}
+	public void testget_V() {
+
+	}
+
+}
