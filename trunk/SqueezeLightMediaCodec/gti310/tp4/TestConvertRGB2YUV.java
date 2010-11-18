7c008197b676492dbb3da0398b6b6aed5823d9e3
chebli.r@gmail.com <chebli.r@gmail.com@11dbf93d-3370-3516-7c29-4c7ee8b01536>
2010-11-18 20:12:57 +0000

    Enelever les <static> partout dans la classe RGB2YUV Continuation de la classe de test

--- trunk/SqueezeLightMediaCodec/gti310/tp4/TestConvertRGB2YUV.java
+++ trunk/SqueezeLightMediaCodec/gti310/tp4/TestConvertRGB2YUV.java
@@ -3,14 +3,20 @@ package gti310.tp4;
 import junit.framework.TestCase;
 
 public class TestConvertRGB2YUV extends TestCase {
-	
+
+	ConvertRGB2YUV testeur=new ConvertRGB2YUV();
+
+	/**
+	 * Test la création de la matrice nulle par le constructeur
+	 * par défaut
+	 */
 	public void testConvertRGB2YUV(){
-		
-		ConvertRGB2YUV testeur=new ConvertRGB2YUV();
-		
-		
+		assertEquals(testeur.getMatriceYUV(),null);
 	}
 	public void testconvert_RGB_to_YUV(){
+		int R = 250 ;
+		int G = 125;
+		int B = 75;
 		
 	}
 	public void testget_Y() {
